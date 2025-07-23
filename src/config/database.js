c04bfa027ede2d3ddf650f37da47ee64584b4d15
Mateus Bastos <mtsbastos18@gmail.com>
2025-07-22 22:30:13 -0300

    alterada a conexao com o mongodb

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.set('strictQuery', true);
-module.exports = mongoose.connect('mongodb://localhost/mymoney')
+module.exports = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mymoney')
+
